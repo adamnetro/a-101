d33dd3e5285c7043f4a1516da191ee22c55ff0bd
adamnetro <64706091+adamnetro@users.noreply.github.com>
2023-02-04 19:53:53 +0100

    Add files via upload

--- a=z.js
+++ a=z.js
@@ -1,7 +1,7 @@
 var Box = document.querySelector('input');
 var Btn = document.querySelector('.btn');
-var Btn2 = document.querySelector('.btn2');
 var result = document.querySelector('.result');
+var Copy = document.querySelector('.copy');
 
 
 function AZ(){
@@ -44,51 +44,109 @@ Btn.addEventListener('click', () => {
     if(Box.value){
     result.value = tarmiz(Box);
     result.style.color = "rgb(0, 255, 0)"
+    Copy.style.display = "block"
 }
 });
 }
 
 
-function deletFT() {
-    var deletWD = document.querySelector('.dlt');
+function A又(){
+
+    Box.placeholder = "Enter your word A => 又"
+
+let romoz = {
+    a :"又",
+    b :"厂",
+    c :"卩", 
+    d :"卜",
+    e :"厶",
+    f :"匸",
+    g :"匚",
+    h :"匕",
+    i :"勹",
+    j :"力",
+    k :"刀",
+    l :"凵",
+    m :"几",
+    n :"冫",
+    o :"冖",
+    p :"冂",
+    q :"八",
+    r :"入",
+    s :"儿",
+    t :"人",
+    u :"亠",
+    v :"二",
+    w :"亅",
+    x :"乛",
+    y :"丨",
+    z :"一",
+}
+
+
+var tarmiz = Box => Box.value.replace(/[a-zA-Z]/g, letter => romoz[letter.toLowerCase()] || letter);
+
+Btn.addEventListener('click', () => {
     if(Box.value){
-        deletWD.style.display = "block"
-    }
+    result.value = tarmiz(Box);
+    result.style.color = "rgb(0, 255, 0)"
+    Copy.style.display = "block"
+}
+});
+}
 
-    if(!Box.value){
-        deletWD.style.display = "none"
-    }
+function 又A(){
 
-    deletWD.addEventListener('click', () => {
-        Box.value = ""
-        deletWD.style.display = "none"
-    })
+    Box.placeholder = "Enter your word 又 => A"
 
-  }
-  
-Box.addEventListener('input', () => {
-    deletFT()
-})
-  
+let romoz = {
+    又 :"a",
+    厂 :"b",
+    卩 :"c", 
+    卜 :"d",
+    厶 :"e",
+    匸 :"f",
+    匚 :"g",
+    匕 :"h",
+    勹 :"i",
+    力 :"j",
+    刀 :"k",
+    凵 :"l",
+    几 :"m",
+    冫 :"n",
+    冖 :"o",
+    冂 :"p",
+    八 :"q",
+    入 :"r",
+    儿 :"s",
+    人 :"t",
+    亠 :"u",
+    二 :"v",
+    亅 :"w",
+    乛 :"x",
+    丨 :"y",
+    一 :"z",
+}
 
 
+var tarmiz = Box => Box.value.replace(/[一-又一-又]/g, letter => romoz[letter.toLowerCase()] || letter);
+
+Btn.addEventListener('click', () => {
+    if(Box.value){
+    result.value = (tarmiz(Box));
+    result.style.color = "rgb(0, 255, 0)"
+    Copy.style.display = "block"
+}
+});
+}
 
 window.onload = function(){
     AZ();
+    cd1.focus()
 }
 
 
 
-Btn.addEventListener('click', () => {
-    if(!Box.value){
-        result.value = "Enter your words"
-        result.style.color = "red"
-        result.style.animationName = "mabritch";
-        let audio = new Audio("errorSD.mp3");
-        audio.play()
-        setTimeout( function(){
-            result.style.animationName = "none";
-        },200);
-    }
-    
-});
+
+
+
---
+++ other-act.js
@@ -0,0 +1,79 @@
+var Box = document.querySelector('input');
+var Btn = document.querySelector('.btn');
+var result = document.querySelector('.result');
+
+
+
+function deletFT() {
+    var deletWD = document.querySelector('.dlt');
+    if(Box.value){
+        deletWD.style.display = "block"
+    }
+
+    if(!Box.value){
+        deletWD.style.display = "none"
+    }
+
+    deletWD.addEventListener('click', () => {
+        Box.value = ""
+        deletWD.style.display = "none"
+    })
+  }
+  
+Box.addEventListener('input', () => {
+    deletFT()
+})
+  
+
+
+
+Btn.addEventListener('click', () => {
+    if(!Box.value){
+        result.value = "Enter your words"
+        result.style.color = "red"
+        result.style.animationName = "mabritch";
+        let audio = new Audio("errorSD.mp3");
+        audio.play()
+        setTimeout( function(){
+            result.style.animationName = "none";
+        },200);
+    }
+});
+
+let cd1 = document.querySelector('.cd1');
+let cd2 = document.querySelector('.cd2');
+
+cd1.addEventListener('click', () => {
+    AZ()
+})
+
+cd2.addEventListener('click', () => {
+    A又()
+})
+
+var swtch = document.querySelector(".switch");
+
+var dwaraA = false;
+
+function dwara(){
+    if(!dwaraA){
+        又A(); 
+        dwaraA = true;
+    } else {
+        A又()
+        dwaraA = false;
+    }
+    swtch.classList.toggle('switchG');
+}
+
+var Copy = document.querySelector('.copy');
+
+Copy.addEventListener('click', function(){
+    navigator.clipboard.writeText(result.value)
+    result.value = "Has been copied !! :)"
+    result.style.color = "orange"
+    Copy.style.display = "none"
+})
+
+
+
